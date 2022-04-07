dd8345bf917f887bcef9827677b999d987419537
Dinis Pereira <diniscoutop@gmail.com>
2022-04-07 21:25:38 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
-import Graph from './components/Graph';
-import Graph1 from './components/Graph1';
-import { useForm } from "react-hook-form";
+import { useState } from 'react';
+import { FileUploader } from './components/FileUploader';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Preview } from './components/Preview';
 
+import './App.css';
 
 function App() {
-  const { register, handleSubmit } = useForm() 
-
-  const onSubmit = (data) => {
-    console.log(data)
-  }
+    const [files, setFiles] = useState([]);
+    const onSuccess = (savedFiles) => {
+        setFiles(savedFiles)
+    };
 
   return (
-
-    
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input ref={register} type="file" name="picture" />
-      <button>Submit</button>
-    </form>
-
-    // <div>
-    //   <Graph1/>
-    // </div>
-
+    <div className="App">
+      <FileUploader onSuccess={onSuccess}/>
+      <Preview files={files}/>
+      <ToastContainer/>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import createRoot from 'react-dom';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-ReactDOM.render(
+createRoot(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
