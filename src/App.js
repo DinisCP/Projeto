1215b681d7fdb1f71140937a521526391cf2b045
Dinis Pereira <diniscoutop@gmail.com>
2022-04-14 18:55:53 +0100

    upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-import React, { Component } from 'react';
 import './App.css';
-import Graph from './components/Graph';
-import Graph1 from './components/Graph1';
-import { useForm } from "react-hook-form";
 
+import DropFileInput from './components/drop-file-input/DropFileInput';
 
 function App() {
-  const { register, handleSubmit } = useForm() 
 
-  const onSubmit = (data) => {
-    console.log(data)
-  }
+    const onFileChange = (files) => {
+        console.log(files);
+    }
 
-  return (
-
-    
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input ref={register} type="file" name="picture" />
-      <button>Submit</button>
-    </form>
-
-    // <div>
-    //   <Graph1/>
-    // </div>
-
-  );
+    return (
+        <div className="box">
+            <h2 className="header">
+                React drop files input
+            </h2>
+            <DropFileInput
+                onFileChange={(files) => onFileChange(files)}
+            />
+        </div>
+    );
 }
 
 export default App;
