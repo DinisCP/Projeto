85105f1a4430f885ccaa6f6ffd16d1b11a9fa3e2
Dinis Pereira <diniscoutop@gmail.com>
2022-05-10 16:11:45 +0100

    ..

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+
+app.use(fileUpload());
+
+// Upload Endpoint
+app.post('/upload', (req, res) => {
+  if (req.items === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
+  }
 
   const item = req.items.item;
 
@@ -10,4 +22,6 @@
     res.json({ itemName: item.name, itemPath: `/uploads/${item.name}` });
   });
 
+});
 
+app.listen(5000, () => console.log('Server Started...'));
